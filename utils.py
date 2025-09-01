d95b6e40a4032a3cbe8e68dfca358224d0f99ed2
Murari Venkata Sai Manoj <saimnj357@gmail.com>
2025-09-01 16:13:24 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 def add(a, b):
-    """This function adds two numbers."""
+    """This function adds two numbers,where a,b are two inputs given by user"""
     return a + b
 
 def subtract(a, b):
-    """This function subtracts two numbers."""
+    """This function subtracts two numbers,where a,b are two inputs given by user"""
     return a - b
 
 def multiply(a, b):
-    """This function multiplies two numbers."""
+    """This function multiplies two numbers.where a,b are two inputs given by user"""
     return a * b
